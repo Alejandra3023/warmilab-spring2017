0d0e4fd2440215759074f6f8c7803826bca23e31
Alejandra Carrasco <acarrascoalayo1@gmail.com>
2017-11-18 12:06:06 -0500

    agregue variables

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,7 +6,32 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  
+
+nombre: string = "Aleja Carrasco";
+ocupacion: string = "Student";
+edad: number = 16
+adjetive: string = "universe"
+
+datos: any = {
+ nombre: "Aleja Carrasco",
+ ocupacion: "student",
+ adjetive: "universe",
+ edad: 16
+
+};
+
+origen: string = "Lima, Peru";
+email: string = "acarrascoalayo1@gmail.com";
+telefono: number = 952876008;
+
+personalinfo: any = {
+origen: "Lima, Peru",
+email: "acarrascoalayo1@gmail.com",
+telefono: 952876008
+
+};
+
+
   constructor(public navCtrl: NavController) {
 
   }
